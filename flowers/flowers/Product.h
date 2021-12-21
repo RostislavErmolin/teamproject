0ca4a5108c04568fc60e6ccb53c3e75790dda712
Ermolin Rostislav <yermolinr@gmail.com>
2021-12-22 00:33:40 +0300

    id produc, show product

--- flowers/flowers/Product.h
+++ flowers/flowers/Product.h
@@ -8,41 +8,66 @@ void getaLine(string& inStr);
 char getaChar();
 class Product {
 private:
+	
 	string name;
 	string description;
 	float price;
 	int count;
+	int id;
 	float OptPrice;
+	
 public:
 	~Product();
-	Product(string n, string d, float p, int c, float o);
+	Product( string n, string d, float p,  int i, int c, float o);
 	string getname();
 	string getdescription();
 	float getprice();
 	int getcount();
+	int getid();
 	float getOptPrice();
 };
 
 class ProductList {
 private:
+	
 	list <Product*> setPtrsProduct; 
 	list <Product*>::iterator iter;
+	int id;
+	string nam;
+	bool ck = false;
 public:
 	~ProductList();
 	void insertProduct(Product*);
-	
 	void display();
+	int getiid();
+	int getidd(int);
+	void Productfind();
 };
+
+/*class ProductLis {
+private:
+	
+	ProductList* ptrProductList;
+public:
+	string renterName; 
+
+};*/
+
+
 
 class ProductAddScreen {
 private:
+	
 	ProductList* ptrProductList;
+	int tid;
 	string tname;
 	string tdescription;
 	float tprice;
 	int tcount;
 	float tOptPrice;
+	
 public:
+	
 	ProductAddScreen(ProductList* ptrPL) : ptrProductList(ptrPL)
 	{
 		
--- flowers/flowers/UserInterface.cpp
+++ flowers/flowers/UserInterface.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <conio.h>
+#define esc 27
 #include "UserInterface.h"
 
-//using namespace std;
 UserInterface::UserInterface()
 {
 	ptrProductList = new ProductList;
@@ -15,26 +15,47 @@ UserInterface::~UserInterface()
 
 void UserInterface::interact()
 {
+	char q;
 	bool c = true;
 	while (c == true) {
+		system("cls");
 		cout << "1-Добавить новый товар" << endl;
 		cout << "2-Список товаров" << endl;
+		cout << "3-Заказать товар" << endl;
+		cout << "esc-Для выхода из программы" << endl;
 		switch (_getch())
 
 		{
 
-		case '1': 
+
+		case '1': {
 			system("cls");
 			ptrProductAddScreen = new ProductAddScreen(ptrProductList);
 			ptrProductAddScreen->setProduct();
 			delete ptrProductAddScreen;
 			break;
-
-		case '2': 
+		}
+		case '2': {
 			system("cls");
 			ptrProductList->display();
+			cout << "\n\nПосмотеть определенный товар?" << endl;
+			cout << "Введите y(yes)/n(no)" << endl;
+			q = _getch();
+			if (q == 'y' || q == 'Y') {
+				ptrProductList->Productfind();
+			}
+			else
+				cout << "Нажми любую клавишу для выхода в меню" << endl;
+			system("pause");
 			break;
-			system("cls");
+		}
+		case esc: {
+			c = false;
+			break; }
+		default:
+			cout << "Неверный ввод!"<<endl;
+			system("pause");
+				break;
 		} 
 	}
 }
